9ebe734b6058ad81cf235948a736d3bc454df564
marlene32100 <katia.debon@hotmail.it>
2020-12-04 10:30:56 +0000

    Added login functionality

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def get_artists():
 def register():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username")})
+            {"username": request.form.get("username").lower()})
 
         if existing_user:
             flash("Username is already in use. Please try another one.")
@@ -49,6 +49,29 @@ def register():
         flash("Well done! Now go ahead and add your profile!")
 
     return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(
+                        request.form.get("username").capitalize()))
+            else:
+                flash("Oops! Check username and password again")
+                return redirect(url_for("login"))
+
+        else:
+            flash("Oops! Check username and password again")
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
