94a21806eb75803e78afadbafcd74bf5b8995cda
marlene32100 <katia.debon@hotmail.it>
2020-12-15 20:36:21 +0000

    Added Javascript functionality on click on cancel button in edit-profile

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def login():
     return render_template("login.html")
 
 
-@app.route("/profile<username>", methods=["GET", "POST"])
+@app.route("/profile/<username>", methods=["GET", "POST"])
 def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
