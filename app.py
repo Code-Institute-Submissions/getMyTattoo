3bff2d801c17d9ae7b54b294c9202e9fe31112cb
marlene32100 <katia.debon@hotmail.it>
2020-12-16 21:56:32 +0000

    Fixed html after validation

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (Flask, flash,
-    render_template, redirect, request,
-    session, url_for)
+                   render_template, redirect, request,
+                   session, url_for)
 from flask_pymongo import PyMongo
 from flask_bootstrap import Bootstrap
 from bson.objectid import ObjectId
@@ -79,12 +79,12 @@ def login():
 
         if existing_user:
             if check_password_hash(
-                existing_user["password"], request.form.get("password")):
-                    session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(
-                        request.form.get("username").capitalize()))
-                    return redirect(
-                        url_for("profile", username=session["user"]))
+                    existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                flash("Welcome, {}".format(
+                    request.form.get("username").capitalize()))
+                return redirect(
+                    url_for("profile", username=session["user"]))
             else:
                 flash("Oops! Check username and password again")
                 return redirect(url_for("login"))
