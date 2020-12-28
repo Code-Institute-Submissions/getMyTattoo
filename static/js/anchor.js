bf63ffd5699a109b83ce2dfd8ff96a317ea8b841
marlene32100 <katia.debon@hotmail.it>
2020-12-28 20:21:35 +0000

    Changed code in anchor.js

--- static/js/anchor.js
+++ static/js/anchor.js
@@ -1,7 +1,10 @@
 // If browser if Firefox, go to the top of the page. This should fix the bug with anchor link in Firefox.
 function anchor() {
+
     if (navigator.userAgent.indexOf("Firefox") != -1) {
-        console.log("Mozilla Firefox detected")
-        document.getElementById("navigation").scrollIntoView();
+
+        console.log("Mozilla Firefox detected");
+
+        window.scrollTo(0, 0);
     }
 }
